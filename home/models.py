0413982edae42eaba491ba325c785797e7c3cbab
javad <javad.gheisarii@gmail.com>
2022-08-21 22:40:47 +0430

    add get_absolute_url

--- home/models.py
+++ home/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 
 class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_posts')
     body = models.TextField()
     date_create = models.DateTimeField(auto_now_add=True)
     date_update = models.DateTimeField(auto_now=True)
-    slug = models.CharField(max_length=50)
+    slug = models.CharField(max_length=64)
 
     def __str__(self):
         return f'{self.slug} - {self.date_update}'
+
+    def get_absolute_url(self):
+        return reverse("home:post_detail", args=(self.id, self.slug))
+    
--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth.models import User
 from .models import Post
 
 
